d9e6d72406d9c151d67a3348ef0372d87326ae38
hugbed <hugo_bedard@hotmail.com>
2016-02-01 16:32:06 -0500

    Update and rename temp.txt to inout.cpp
    Add in out code

---
+++ Exercice1/inout.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+}
+
+/*
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!";
+}
+
+// ----------------
+
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+	String hello("Hello world!");
+	cout << hello;
+}
+
+// -----------------
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	String message;
+	cin >> message;
+	cout << message;
+}
+
+*/
